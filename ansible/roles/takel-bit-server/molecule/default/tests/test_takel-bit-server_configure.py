af9e6a9583110c7c5276cf04075a19b826bd612c
Stephan Wehrheim <swehrheim@geospin.de>
2020-03-28 01:03:18 +0100

    Add unit tests for takel-bit-server

--- ansible/roles/takel-bit-server/molecule/default/tests/test_takel-bit-server_configure.py
+++ ansible/roles/takel-bit-server/molecule/default/tests/test_takel-bit-server_configure.py
@@ -5,5 +5,10 @@ testinfra_hosts = testaid.hosts()
 
 def test_takel_bit_server_configure(host, testvars):
     repo_dir = testvars['takel_bit_server_repo_dir']
+    dir = host.file(repo_dir)
 
-    assert host.file(repo_dir).is_directory
+    assert dir.exists
+    assert dir.is_directory
+    assert dir.user == testvars['takel_bit_server_bit_user']
+    assert dir.group == testvars['takel_bit_server_bit_group']
+    assert dir.mode == 0o755
--- ansible/roles/takel-bit-server/molecule/default/tests/test_takel-bit-server_user.py
+++ ansible/roles/takel-bit-server/molecule/default/tests/test_takel-bit-server_user.py
@@ -3,8 +3,18 @@ import testaid
 testinfra_hosts = testaid.hosts()
 
 
-def test_takel_bit_server_user(host, testvars):
+def test_takel_bit_server_group(host, testvars):
+    bit_group_name = testvars['takel_bit_server_bit_group']
+    bit_group_gid = testvars['takel_bit_server_bit_gid']
+    bit_group = host.group(bit_group_name)
+
+    assert bit_group.exists
+    assert bit_group.gid == bit_group_gid
+
+
+def test_takel_bit_server_ssh(host, testvars):
     bit_username = testvars['takel_bit_server_bit_user']
     authorized_keys_file = '/home/' + bit_username + '/.ssh/authorized_keys'
+
     for key in testvars['takel_bit_server_sshpubkeys_custom']:
         assert key in host.file(authorized_keys_file).content_string
