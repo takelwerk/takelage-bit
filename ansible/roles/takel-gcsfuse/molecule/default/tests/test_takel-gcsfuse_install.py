9869fac2247696d26629e4260c5819d7395cafdd
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-02 18:38:58 +0200

    Make gcsfuse package test optional

--- ansible/roles/takel-gcsfuse/molecule/default/tests/test_takel-gcsfuse_install.py
+++ ansible/roles/takel-gcsfuse/molecule/default/tests/test_takel-gcsfuse_install.py
@@ -4,9 +4,10 @@ testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_gcsfuse_install_deb_packages_installed(host, testvars):
-    install_packages = testvars['takel_gcsfuse_deb_install_packages']
+    if 'takel_gcsfuse_gcs_bucket_key' in testvars:
+        install_packages = testvars['takel_gcsfuse_deb_install_packages']
 
-    for install_package in install_packages:
-        package = host.package(install_package)
+        for install_package in install_packages:
+            package = host.package(install_package)
 
-        assert package.is_installed
+            assert package.is_installed
