2fbf18d65c61e66771bf5afe92b161b890f2dc89
Stephan Wehrheim <swehrheim@geospin.de>
2020-04-10 19:24:51 +0000

    Migrate to molecule 3

--- ansible/roles/takel-base/molecule/default/tests/test_takel-base_configure.py
+++ ansible/roles/takel-base/molecule/default/tests/test_takel-base_configure.py
@@ -3,28 +3,15 @@ import takeltest
 testinfra_hosts = takeltest.hosts()
 
 
-def test_takel_base_configure_locale(host):
+def test_takel_base_configure_locales(host, testvars):
     file = host.file('/etc/default/locale')
+    locales = testvars['takel_base_locales']
+
     assert file.exists
     assert file.is_file
     assert file.user == 'root'
     assert file.group == 'root'
     assert file.mode == 0o644
 
-
-def test_takel_base_configure_20auto_upgrades(host):
-    file = host.file('/etc/apt/apt.conf.d/20auto-upgrades')
-    assert file.exists
-    assert file.is_file
-    assert file.user == 'root'
-    assert file.group == 'root'
-    assert file.mode == 0o644
-
-
-def test_takel_base_configure_50unattended_upgrades(host):
-    file = host.file('/etc/apt/apt.conf.d/50unattended-upgrades')
-    assert file.exists
-    assert file.is_file
-    assert file.user == 'root'
-    assert file.group == 'root'
-    assert file.mode == 0o644
+    for locale in locales:
+        assert locale in file.content_string
