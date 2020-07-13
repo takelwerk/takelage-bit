c7859e711120dd0ea91edf0450c674ed2d81769a
Stephan Wehrheim <swehrheim@geospin.de>
2020-07-13 15:18:13 +0200

    Refactor bit components

---
+++ rake/local/docker/takelbase/lib/local.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rake'
+
+@cmd_local_docker_takelbase_molecule_command = \
+  'cd ansible && ' \
+  'TAKELAGE_PROJECT_ENV=%<project_environment>s ' \
+  'molecule %<molecule_command>s --scenario-name local'
+
+@cmd_local_docker_takelbase_molecule_features = \
+  'cd ansible && ' \
+  'TAKELAGE_PROJECT_ENV=%<project_environment>s ' \
+  'molecule dependency --scenario-name local'
+
+@cmd_local_docker_takelbase_list = {
+  converge: @cmd_local_docker_takelbase_molecule_command,
+  destroy: @cmd_local_docker_takelbase_molecule_command,
+  login: @cmd_local_docker_takelbase_molecule_command,
+  verify: @cmd_local_docker_takelbase_molecule_command,
+  features: @cmd_local_docker_takelbase_molecule_features
+}
