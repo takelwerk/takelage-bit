7952a1cd534f754b7a6cdbbddce0a875f5dee1bb
Stephan Wehrheim <swehrheim@geospin.de>
2021-02-03 18:53:52 +0100

    tau pull

---
+++ ansible/roles/takel-gcsfuse/molecule/default/system/test_takel-gcsfs_system.py
@@ -0,0 +1,21 @@
+import takeltest
+from pathlib import Path
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_gcsfs_system_bin_path(host):
+    # https://github.com/GoogleCloudPlatform/gcsfuse/blob/master/tools/mount_gcsfuse/find.go
+    # HACK(jacobsa): Since mount(8) appears to call its helpers with $PATH
+    # unset, I can find no better way to do this than searching a hard-coded
+    # list of candidates. However, include as a candidate the $PATH-relative
+    # version in case we are being called in a context with $PATH set, such as
+    # a test.
+    valid_bin_paths = [Path('/usr/bin'), Path('/usr/local/bin')]
+    bin_path = host.check_output('which gcsfuse')
+    assert Path(bin_path).parents[0] in valid_bin_paths
+
+
+def test_takel_gcsfs_system_called_by_mount(host):
+    result = host.run('mount -a')
+    assert "findGcsfuse: Can't find a usable executable." not in result.stderr
