aeb881a298ec76751835403b8d6b77cff25e8ba4
Cucumber <cucumber@example.com>
2021-02-08 16:17:56 +0100

    Skip tests if takel_gcsfuse_gcs_bucket_key is undefined

--- ansible/roles/takel-gcsfuse/molecule/default/system/test_takel-gcsfs_system.py
+++ ansible/roles/takel-gcsfuse/molecule/default/system/test_takel-gcsfs_system.py
@@ -1,3 +1,4 @@
+import pytest
 import takeltest
 from pathlib import Path
 
@@ -5,6 +6,9 @@ testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_gcsfs_system_bin_path(host, testvars):
+    if 'takel_gcsfuse_gcs_bucket_key' not in testvars:
+        pytest.skip('takel_gcsfuse_gcs_bucket_key')
+
     # https://github.com/GoogleCloudPlatform
     # /gcsfuse/blob/master/tools/mount_gcsfuse/find.go
     # HACK(jacobsa): Since mount(8) appears to call its helpers with $PATH
@@ -12,16 +16,16 @@ def test_takel_gcsfs_system_bin_path(host, testvars):
     # list of candidates. However, include as a candidate the $PATH-relative
     # version in case we are being called in a context with $PATH set,
     # such as a test.
-    if 'takel_gcsfuse_gcs_bucket_key' in testvars:
-        valid_bin_paths = [Path('/usr/bin'), Path('/usr/local/bin')]
-        bin_path = host.check_output('which gcsfuse')
+    valid_bin_paths = [Path('/usr/bin'), Path('/usr/local/bin')]
+    bin_path = host.check_output('which gcsfuse')
 
-        assert Path(bin_path).parents[0] in valid_bin_paths
+    assert Path(bin_path).parents[0] in valid_bin_paths
 
 
 def test_takel_gcsfs_system_called_by_mount(host, testvars):
-    if 'takel_gcsfuse_gcs_bucket_key' in testvars:
-        result = host.run('mount -a')
+    if 'takel_gcsfuse_gcs_bucket_key' not in testvars:
+        pytest.skip('takel_gcsfuse_gcs_bucket_key')
 
-        assert "findGcsfuse: Can't find a usable executable." \
-               not in result.stderr
+    result = host.run('mount -a')
+
+    assert "findGcsfuse: Can't find a usable executable." not in result.stderr
--- ansible/roles/takel-gcsfuse/molecule/default/tests/test_takel-gcsfuse_install.py
+++ ansible/roles/takel-gcsfuse/molecule/default/tests/test_takel-gcsfuse_install.py
@@ -1,13 +1,16 @@
+import pytest
 import takeltest
 
 testinfra_hosts = takeltest.hosts()
 
 
 def test_takel_gcsfuse_install_deb_packages_installed(host, testvars):
-    if 'takel_gcsfuse_gcs_bucket_key' in testvars:
-        install_packages = testvars['takel_gcsfuse_deb_install_packages']
+    if 'takel_gcsfuse_gcs_bucket_key' not in testvars:
+        pytest.skip('takel_gcsfuse_gcs_bucket_key')
 
-        for install_package in install_packages:
-            package = host.package(install_package)
+    install_packages = testvars['takel_gcsfuse_deb_install_packages']
 
-            assert package.is_installed
+    for install_package in install_packages:
+        package = host.package(install_package)
+
+        assert package.is_installed
