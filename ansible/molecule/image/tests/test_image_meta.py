8e6de934b0ada6fe6dc4a31e12881b4924c53148
Stephan Wehrheim <swehrheim@geospin.de>
2021-06-01 15:48:47 +0200

    Fix vars path

--- ansible/molecule/image/tests/test_image_meta.py
+++ ansible/molecule/image/tests/test_image_meta.py
@@ -24,8 +24,8 @@ def test_image_meta_env_values(image_meta_data, env, value):
 
 def test_image_meta_cmd(testvars, image_meta_data):
     image = os.environ.get('TAKELAGE_PROJECT_IMG')
-    if 'command' in testvars['project'][image]:
-        expected = testvars['project'][image]['command']
+    if 'command' in testvars['project']['images'][image]:
+        expected = testvars['project']['images'][image]['command']
     else:
         expected = '/usr/bin/tail -f /dev/null'
     assert expected.split(' ') == image_meta_data['Config']['Cmd']
